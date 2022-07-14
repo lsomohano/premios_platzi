f09e68f4c28edd29cb2b0fb0b0ab151da538e6c5
lsomohano <lsomohano@avasa.com.mx>
2022-07-14 18:09:59 -0500

    Cambio en vistas

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,48 @@
+from msilib.schema import Class
+from re import template
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question,Choice
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request, "polls/index.html", {
-        "latest_question_list":latest_question_list
-    })
+#def index(request):
+#   latest_question_list = Question.objects.all()
+#    return render(request, "polls/index.html", {
+#        "latest_question_list":latest_question_list
+#    })
 
 
-def detail(request, question_id):
-    question=get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html",{
-        "question":question
-    })
+#def detail(request, question_id):
+#    question=get_object_or_404(Question, pk=question_id)
+#    return render(request, "polls/detail.html",{
+#        "question":question
+#    })
 
-def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, "polls/results.html",{
+#            "question":question        })
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "lastest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.ListView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultView(generic.ListView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
