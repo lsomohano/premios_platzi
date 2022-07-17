f0f16a75ec2398a18409b1299ea15656647d45ed
lsomohano <lsomohano20@hotmail.com>
2022-07-17 00:46:21 -0500

    Modificando el admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,16 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register([Question,Choice])
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline,]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
 #admin.site.register(Choice)
